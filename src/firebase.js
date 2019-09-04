c8ef36d31a164e162ad85bae99676144f2349797
nateschillen <54914760+nateschillen@users.noreply.github.com>
2019-09-04 15:27:34 -0600

    Update Firebase Config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
+  apiKey: "AIzaSyDvpGVFk30U3lhefZX-mGCjvQiXSddH25s",
+  authDomain: "authentication-c398a.firebaseapp.com",
+  databaseURL: "https://authentication-c398a.firebaseio.com",
+  projectId: "authentication-c398a",
   storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  messagingSenderId: "815408671129",
+  appId: "1:815408671129:web:220f040305c6f8bae6cb08"
 };
 
 firebase.initializeApp(config);
