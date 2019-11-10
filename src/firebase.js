a6c43dc44f713ace22205722ef383e57c3b68877
nateschillen <54914760+nateschillen@users.noreply.github.com>
2019-11-10 16:16:53 -0700

    healthtracker

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyDvpGVFk30U3lhefZX-mGCjvQiXSddH25s",
-  authDomain: "authentication-c398a.firebaseapp.com",
-  databaseURL: "https://authentication-c398a.firebaseio.com",
-  projectId: "authentication-c398a",
-  storageBucket: "",
-  messagingSenderId: "815408671129",
-  appId: "1:815408671129:web:220f040305c6f8bae6cb08"
+  apiKey: "AIzaSyCtucDEpMtr7kgDvdHAaBLzAlTWxXsLaTY",
+  authDomain: "healthtracker-fb4b2.firebaseapp.com",
+  databaseURL: "https://healthtracker-fb4b2.firebaseio.com",
+  projectId: "healthtracker-fb4b2",
+  storageBucket: "healthtracker-fb4b2.appspot.com",
+  messagingSenderId: "636344526414",
+  appId: "1:636344526414:web:a4c13ce52555c15752851f"
 };
 
 firebase.initializeApp(config);
